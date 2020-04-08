5086cc5f43a26ac2bfe17f01941ed4b21169425a
eric-boettner <boettner.eric@gmail.com>
2020-04-07 19:26:02 -0700

    Small tweaks for testing

--- test_cache_client.cc
+++ test_cache_client.cc
@@ -8,20 +8,28 @@ int main() {
     try {
         Cache cache("127.0.0.1","4000");
         std::cout << "Connection sucsessful" << std::endl;
-        cache.set("hello","two", 1);
+        cache.set("hello","two", 3);
         cache.set("me","10", 2);
         cache.set("too","2", 1);
         std::cout << "SET sucsessful" << std::endl;
         Cache::size_type s;
-        std::cout << "GET sucsessful : " << cache.get("hello", s) << std::endl;
         std::string res = "two";
         assert(cache.get("hello", s) == res);
+        res = "10";
+        assert(cache.get("me", s) == res);
+        res = "2";
+        assert(cache.get("too", s) == res);
+        std::cout << "GET sucsessful" << std::endl;
         assert(cache.del("hello") == true);
+        assert(cache.get("hello", s) == nullptr);
         assert(cache.del("empty") == false);
         std::cout << "DEL sucsessful" << std::endl;
-        assert(cache.space_used() == 3);
+        assert(cache.space_used() == 3); // "10","2"
         std::cout << "Space_used sucsessful" << std::endl;
         cache.reset();
+        assert(cache.get("hello", s) == nullptr);
+        assert(cache.get("me", s) == nullptr);
+        assert(cache.get("too", s) == nullptr);
         std::cout << "RESET sucsessful" << std::endl;
     } catch(std::exception const& e){
         std::cerr << "Error: " << e.what() << std::endl;
